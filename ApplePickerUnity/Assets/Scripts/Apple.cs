5d9f349279f76a4b17bce152959d618854d20fe0
samuel-ricketts <78981392+samuel-ricketts@users.noreply.github.com>
2022-02-07 16:50:20 -0600

    ApplePicker Finish
    Finished up everything

--- ApplePickerUnity/Assets/Scripts/Apple.cs
+++ ApplePickerUnity/Assets/Scripts/Apple.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
-    public static float bottomY = -10f;
+    public static float bottomY = -20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +22,12 @@ public class Apple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < bottomY)
+        if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
-        }
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+        } 
+
     }
 }
